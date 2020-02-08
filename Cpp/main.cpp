662f8b4c2562fff73222177d85f307c7fb784836
poc1673Laptop <petercaya@gmail.com>
2020-02-08 07:10:05 -0700

    Added cpp folder.

---
+++ Cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+	int test_array [5] = {3,6,1,2,4};
+	for(int i = 0 ; i <= 4 ; i++ ){
+	   cout << test_array[i] << endl; // prints Hello World		
+	   }
+
+   return 0;
+}
